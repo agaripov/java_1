64a2a7e2d9bc8d4568165c7ee03d5f07a0c3a8ec
agaripov <agaripov@lanit.ru>
2019-06-19 01:19:44 +0500

    Задание №2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/myfirst.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/myfirst.java
@@ -11,11 +11,18 @@ public class Myfirst {
 
         Rectangle r = new Rectangle(4,6);
         System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+        Point p1 = new Point(3,4);
+        System.out.println("Координаты первой точки: X = " + p1.x + "; Y = " + p1.y);
+
+        Point p2 = new Point(6,7);
+        System.out.println("Координаты второй точки: X = " + p2.x + "; Y = " + p2.y);
+
+        System.out.println("Расстояние между точками равно " + p1.distance(p2));
     }
 
     public static void hello(String somebody) {
         System.out.println("hello " + somebody + "!");
     }
-
 
 }
