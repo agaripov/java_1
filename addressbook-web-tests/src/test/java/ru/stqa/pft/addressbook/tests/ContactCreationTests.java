d9bbe6240d943a706b1fff7dd7d7a7888ba2f808
agaripov <agaripov@lanit.ru>
2019-07-20 20:44:07 +0500

    задание 12 и 13: генераторы тестовых данных и пераметризованные тесты

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,39 +1,61 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.thoughtworks.xstream.XStream;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-  @Test
-  public void testContactCreation() {
-    app.goTo().HomePage();
-    Contacts before = app.contact().all();
-    File photo = new File("src/test/resources/1.jpg");
-    ContactData contact = new ContactData().withFirstname("First name").withLastname("Last name").withPhoto(photo);
-    app.contact().create(contact);
-    assertThat(app.contact().count(), equalTo(before.size() + 1));
-    Contacts after = app.contact().all();
-    assertThat(after, equalTo(
-            before.withAdded(contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
-  }
+    @DataProvider
+    public Iterator<Object[]> validContacts() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
+        String xml = "";
+        String line = reader.readLine();
+        while (line != null) {
+            xml += line;
+            line = reader.readLine();
+        }
+        XStream xstream = new XStream();
+        xstream.processAnnotations(ContactData.class);
+        List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
+        return contacts.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
+    }
 
-  @Test
-  public void testContactBadCreation() {
-    app.goTo().HomePage();
-    Contacts before = app.contact().all();
-    ContactData contact = new ContactData().withFirstname("First name'").withLastname("Last name'");
-    app.contact().create(contact);
-    assertThat(app.contact().count(), equalTo(before.size()));
-    Contacts after = app.contact().all();
-    assertThat(after, equalTo(before));
-  }
+    @Test(dataProvider = "validContacts")
+    public void testContactCreation(ContactData contact) {
+        app.goTo().HomePage();
+        Contacts before = app.contact().all();
+        File photo = new File("src/test/resources/1.jpg");
+        app.contact().create(contact);
+        assertThat(app.contact().count(), equalTo(before.size() + 1));
+        Contacts after = app.contact().all();
+        assertThat(after, equalTo(
+                before.withAdded(contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+    }
+
+    @Test
+    public void testContactBadCreation() {
+        app.goTo().HomePage();
+        Contacts before = app.contact().all();
+        ContactData contact = new ContactData().withFirstname("First name'").withLastname("Last name'");
+        app.contact().create(contact);
+        assertThat(app.contact().count(), equalTo(before.size()));
+        Contacts after = app.contact().all();
+        assertThat(after, equalTo(before));
+    }
 
 
 }
