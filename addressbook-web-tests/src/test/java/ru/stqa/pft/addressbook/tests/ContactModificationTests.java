1567d75f434773a74ca264c76463e851e77f1e11
agaripov <agaripov@lanit.ru>
2019-07-20 21:44:45 +0500

    сделали конфигурационные файлы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -8,7 +8,7 @@ import ru.stqa.pft.addressbook.model.Contacts;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class ContactModificationTest extends TestBase {
+public class ContactModificationTests extends TestBase {
 
     @BeforeMethod
     public void ensurePreconditions() {
